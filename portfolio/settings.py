407c301a05724944c2e642ff0adfe155a9e66fbb
Choulla Mohammed <mohammedweb4@gmail.com>
2018-08-23 16:18:18 +0100

    adding postgresql to my project

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'9r3atlma1999',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -121,4 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/' 
+#fblast media momkin dir ay hajams media li fo9 mat9ishach
