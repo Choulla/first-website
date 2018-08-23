f291eff448b5f505d6d143e9069f9cb1ce3a72b6
Choulla Mohammed <mohammedweb4@gmail.com>
2018-08-24 00:21:41 +0100

    adding our jobs to the html code and using bootstrp

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	title = models.CharField(max_length=255)
+	pub_date = models.DateTimeField(null=True)
+	#auto_now=False
+	body = models.TextField(null=True)
+	image = models.ImageField(upload_to='images/')
+
+
+	
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings #import settings , medi_url , medi_root
 from django.conf.urls.static import static #import the hall app , static
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
